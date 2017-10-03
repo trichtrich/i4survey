bc41caf6be4aaeef7779078e9d831338424cf89f
gust93tb <gust93tb@gmail.com>
2017-10-03 15:34:35 +0700

    ...

--- models/i4s_data_item.py
+++ models/i4s_data_item.py
@@ -2,14 +2,20 @@
 
 from odoo import models, fields, api
 
-# class i4survey(models.Model):
-#     _name = 'i4survey.i4survey'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+class i4SDaTaItem(models.Model):
+	datagroupid = fields.Integer('datagroupid', track_visibility='always')
+	code = fields.Char(string='CODE')
+	node_1 = fields.Char(string='NODE_1')
+	node_2 = fields.Char(string='NODE_2')
+	node_3 = fields.Char(string='NODE_3')
+	node_4 = fields.Char(string='NODE_4')
+	level = fields.Integer()
+	name = fields.Text(string='name')
+	description = fields.Text(string='Mô tả')
+	display = fields.Char(string='Hiển thị')
+	status = fields.Integer(string='Trạng thái')
+	displayorder = fields.Integer(string='displayorder')
+	validatefrom = fields.Datetime(string='VALIDATEDFROM', readonly=True)
+	validateto = fields.Datetime(string='VALIDATEDTO', readonly=True)
+	result = fields.Char(string='result')
+	isSend = fields.Integer(string='isSend')
