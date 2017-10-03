8aa5c30505fd904cbbcdd43be87c45eafca9e738
Tai Le <lttai20@gmail.com>
2017-10-03 14:29:01 +0700

    model doanh nghiep

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import i4s_data_group
+from . import i4s_doanhnghiep
--- models/i4s_doanhnghiep.py
+++ models/i4s_doanhnghiep.py
@@ -2,6 +2,35 @@
 
 from odoo import models, fields, api
 
+class i4s_doanhnghiep(models.Model):
+    _name = 'i4s.doanhnghiep'
+    
+    _rec_name = 'ten_doanhnghiep'
+    
+    ten_doanhnghiep = fields.Text(string='Tên doanh nghiệp')
+    ten_doanhnghiep_en = fields.Text(string='Tên doanh nghiệp (EN)')
+    diachi = fields.Text(string='Địa chỉ')
+    sodienthoai = fields.Text(string='Số điện thoại')
+    so_gpkd = fields.Text(string='Số giấy phép kinh doanh')
+    nguoi_daidien = fields.Text(string='Người đại diện')
+    fax = fields.Text(string='Fax')
+    email = fields.Text(string='Email')
+    maso_thue = fields.Text(string='Mã số thuế')
+    linhvucid = fields.Integer(string='Lĩnh vực')
+    loaihinhid = fields.Integer(string='Loại hình')
+    result = fields.Text(string='Kết quả')
+    is_send = fields.Integer(string='Đã gửi')
+    chien_luoc = fields.Text(string='Chiến lược')
+    lanh_dao = fields.Text(string='Lãnh đạo')
+    san_pham = fields.Text(string='Sản phẩm')
+    khach_hang = fields.Text(string='Khách hàng')
+    van_hanh = fields.Text(string='Vận hành')
+    van_hoa = fields.Text(string='Văn hóa')
+    con_nguoi = fields.Text(string='Con người')
+    chinh_sach = fields.Text(string='Chính sách')
+    cong_nghe = fields.Text(string='Công nghệ')
+    sonhanvien = fields.Integer(string='Số nhân viên')
+    doanhthu = fields.Text(string='Doanh thu')
 # class i4survey(models.Model):
 #     _name = 'i4survey.i4survey'
 
