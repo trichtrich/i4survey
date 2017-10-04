2b40b30ebb0aa8bee6e74a4dae64ee1025ad723a
chuongluke <chuongluke@gmail.com>
2017-10-04 14:45:07 +0700

    update controller method

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -17,7 +17,7 @@ class Website(Website):
 		counttitle = self.search_dataitem('i4s.data.item', 1, '', 1)
 
 		for x in counttitle:
-			x_result = self.search_dataitem('i4s.data.item', 1, x.code, 2)
+			x_result = self.search_dataitem('i4s.data.item', 1, x.node_1, 2)
 			name = x.name
 			image = x.display_image
 			e  = {}
@@ -47,7 +47,7 @@ class Website(Website):
 
 
 
-	def search_dataitem(self, table, datagroupid, code, level):
+	def search_dataitem(self, table, datagroupid, node_1, level):
 
 		domain = []
 
@@ -62,3 +62,44 @@ class Website(Website):
 
 		results = http.request.env[table].sudo().search(domain)
 		return results
+
+
+
+	@http.route('/survey', auth='public')
+	def create_survey(self, **kw):
+		nguoi_daidien = kw.get('nguoi_daidien')
+		sodienthoai = kw.get('sodienthoai')
+		email = kw.get('email')
+		ten_doanhnghiep = kw.get('ten_doanhnghiep')
+		linhvucid = kw.get('linhvucid')
+		sonhanvien = kw.get('sonhanvien')
+		doanhthu = kw.get('doanhthu')
+		diachi = kw.get('diachi')
+
+
+
+		doanhnghiep = http.request.env['i4s.doanhnghiep'].sudo().create({
+			'nguoi_daidien': nguoi_daidien,
+			'sodienthoai': sodienthoai,
+			'email': email,
+			'ten_doanhnghiep': ten_doanhnghiep,
+			'linhvucid': linhvucid,
+			'sonhanvien': sonhanvien,
+			'doanhthu': doanhthu,
+			'diachi': diachi
+		})
+		if doanhnghiep:
+			counttitle = self.search_dataitem('i4s.data.item', 1, '', 1)
+
+			for x in counttitle:
+				x_result = self.search_dataitem('i4s.data.item', 1, x.node_1, 2)
+				for i in x_result:
+
+					answer = kw.get('answer_' + str(i.id))
+
+					http.request.env['i4s.survey.result'].sudo().create({
+						'survey_question_id': str(i.id),
+						'survey_question_code': i.code,
+						'doanhnghiepid': str(doanhnghiep.id),
+						'current': answer
+					})
